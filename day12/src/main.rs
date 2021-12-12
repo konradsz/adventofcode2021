5e3f79234b254dafa34be517f8df50f6d292d8ef
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2021-12-12 20:43:57 +0100

    add solutions to puzzles from day12

---
+++ day12/src/main.rs
@@ -0,0 +1,103 @@
+use std::collections::HashMap;
+
+fn is_small_cave(cave: &str) -> bool {
+    cave == "start" || cave.chars().all(|c| c.is_lowercase())
+}
+
+fn remove_connections_to(cave: &str, connections: &mut HashMap<String, Vec<String>>) {
+    connections.values_mut().for_each(|destination_caves| {
+        destination_caves.retain(|destination_cave| destination_cave != cave)
+    });
+}
+
+fn look_for_path(
+    current_path: Vec<String>,
+    mut connections: HashMap<String, Vec<String>>,
+    mut visited_small_caves: Vec<String>,
+    mut visited_twice: bool,
+    possible_paths: &mut Vec<Vec<String>>,
+) {
+    let current_cave = current_path.last().unwrap();
+    if current_cave == "end" {
+        possible_paths.push(current_path);
+        return;
+    }
+
+    let destinations = if let Some(destinations) = connections.get(current_cave) {
+        destinations.clone()
+    } else {
+        return;
+    };
+
+    if is_small_cave(current_cave) {
+        if !visited_twice && visited_small_caves.contains(current_cave) {
+            visited_twice = true;
+            for vsc in visited_small_caves.iter() {
+                remove_connections_to(vsc, &mut connections);
+                connections.remove(vsc);
+            }
+        }
+
+        if visited_twice || current_cave == "start" {
+            remove_connections_to(current_cave, &mut connections);
+            connections.remove(current_cave);
+        } else {
+            visited_small_caves.push(current_cave.clone());
+        }
+    }
+
+    for destination in destinations {
+        let mut my_path = current_path.clone();
+        my_path.push(destination.clone());
+        look_for_path(
+            my_path.clone(),
+            connections.clone(),
+            visited_small_caves.clone(),
+            visited_twice,
+            possible_paths,
+        );
+    }
+}
+
+fn part_1(connections: HashMap<String, Vec<String>>) -> usize {
+    let mut possible_paths = Vec::new();
+    look_for_path(
+        vec!["start".into()],
+        connections,
+        Vec::new(),
+        true,
+        &mut possible_paths,
+    );
+    possible_paths.len()
+}
+
+fn part_2(connections: HashMap<String, Vec<String>>) -> usize {
+    let mut possible_paths = Vec::new();
+    look_for_path(
+        vec!["start".into()],
+        connections,
+        Vec::new(),
+        false,
+        &mut possible_paths,
+    );
+
+    possible_paths.len()
+}
+
+fn main() {
+    let mut connections: HashMap<String, Vec<String>> = HashMap::new();
+    let input = std::fs::read_to_string("input").unwrap();
+    for line in input.lines() {
+        let mut parts = line.split('-');
+        let (from, to) = (parts.next().unwrap(), parts.next().unwrap());
+
+        connections.entry(from.into()).or_default().push(to.into());
+
+        if from != "start" && to != "end" {
+            connections.entry(to.into()).or_default().push(from.into());
+        }
+    }
+
+    assert_eq!(part_1(connections.clone()), 4773);
+    assert_eq!(part_2(connections.clone()), 116985);
+}
