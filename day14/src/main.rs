7967891eedad62f74b4adcdec319c963881e7767
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2021-12-14 21:31:34 +0100

    fix the bug!

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -4,6 +4,7 @@ fn process(
     steps: usize,
     insertion_rules: &HashMap<String, [String; 2]>,
     mut counts: HashMap<String, usize>,
+    last_char: char,
 ) -> usize {
     for _ in 0..steps {
         let to_increase = counts
@@ -22,14 +23,13 @@ fn process(
     }
 
     let mut char_counts: HashMap<char, usize> = HashMap::new();
+    *char_counts.entry(last_char).or_default() += 1;
     for (pair, count) in counts.iter() {
-        let mut cs = pair.chars();
-        *char_counts.entry(cs.next().unwrap()).or_default() += count;
-        *char_counts.entry(cs.next().unwrap()).or_default() += count;
+        *char_counts.entry(pair.chars().next().unwrap()).or_default() += count;
     }
 
-    let min = char_counts.values().min().unwrap() / 2;
-    let max = char_counts.values().max().unwrap() / 2;
+    let min = char_counts.values().min().unwrap();
+    let max = char_counts.values().max().unwrap();
     max - min
 }
 
@@ -45,6 +45,7 @@ fn main() {
             .entry(String::from_utf8(w.to_vec()).unwrap())
             .or_default() += 1;
     });
+    let last_char = template.chars().last().unwrap();
 
     let insertion_rules: HashMap<String, [String; 2]> = parts
         .next()
@@ -65,8 +66,8 @@ fn main() {
         })
         .collect();
 
-    let part_1 = process(10, &insertion_rules, counts.clone());
-    let part_2 = process(40, &insertion_rules, counts);
-    assert_eq!(part_1 - 1, 2891);
-    assert_eq!(part_2 - 1, 4607749009683);
+    let part_1 = process(10, &insertion_rules, counts.clone(), last_char);
+    let part_2 = process(40, &insertion_rules, counts, last_char);
+    assert_eq!(part_1, 2891);
+    assert_eq!(part_2, 4607749009683);
 }
